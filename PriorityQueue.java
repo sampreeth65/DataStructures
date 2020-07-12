beb2b43151a3b9331ce6a52fe59ce4c72f1f3c7d
sampreeth65 <55499513+sampreeth65@users.noreply.github.com>
2020-07-12 19:31:42 +1000

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -6,14 +6,40 @@ public class PriorityQueue
 {
 	//private Array ar = new Array(10);
 	
-	private int[] items = new int[10];
+	private int[] items = new int[5];
 	private int count;
 	
 	public void add(int item)
 	{
+		if (count == items.length)
+			throw new IllegalStateException();
 		
+		int i;
+		for (i = count; i>= 0; i--)
+		{
+			if (items[i] > item)
+				items[i + 1] = items[i];
+			else 
+				break;
+		}
+		items[i + 1] = item;
+		count++;
 	}
 	
+	/**
+	 * removing based on largest number in the queue.
+	 * @return 
+	 */
+	public int remove()
+	{
+		if (count == 0)
+			throw new IllegalStateException();
+		
+		return items[--count];
+	}
+	
+	
+	@Override
 	public String toString() {
 		return Arrays.toString(items);
 	}
